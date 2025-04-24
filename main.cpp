c93a5ad8cf14a4aae07815151d16f9f531b6f1cc
the-nazim <thenazim0707@gmail.com>
2025-04-24 17:39:55 +0000

    Second commit

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 
 using namespace std;
 
-class Contact
+class PhoneBook
 {
     private:
         string name;
         string phoneNumber;
+        map<string, string> contact;
     
     public:
-        Contact()
+        PhoneBook()
         {
             name = "Unknown";
             phoneNumber = "000-000-0000";
@@ -21,18 +22,33 @@ class Contact
             cin>>name;
             cout<<"Enter phone number: ";
             cin>>phoneNumber; 
+            contact[name] = phoneNumber;
         }
         void show_contacts()
         {
-            cout<<"Name: "<<name<<endl;
-            cout<<"Phone Number: "<<phoneNumber<<endl;
+            // cout<<"Name: "<<name<<endl;
+            // cout<<"Phone Number: "<<phoneNumber<<endl;
+            for(auto i: contact)
+            {
+                cout<<"Name: "<<i.first<<endl;
+                cout<<"Phone Number: "<<i.second<<endl;
+            }
         }
 };
 
 int main()
-{
-    Contact c;
-    c.get_contacts();
-    c.show_contacts();
+{ 
+    PhoneBook pb;
+    bool flag = true;
+    while(flag)
+    {
+        char ch;
+        pb.get_contacts();
+        cout<<"Do you want to add more (Y/n)? ";
+        cin>>ch;
+        if(ch=='n' || ch=='N')
+            flag=false;
+    }
+    pb.show_contacts();
     return 0;
 }
